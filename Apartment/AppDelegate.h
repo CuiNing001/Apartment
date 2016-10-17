7e803f5e294529468f6de3d307a86c1574b00e6c
henghui <henghui@henghuideMacBook-Pro.local>
2016-10-17 17:06:30 +0800

    Initial Commit

---
+++ Apartment/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Apartment
+//
+//  Created by henghui on 2016/10/17.
+//  Copyright © 2016年 henghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
